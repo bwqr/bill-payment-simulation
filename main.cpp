53abd4db3d9943a255ed0a49bcdf43da6ec76495
bwqr <ruzgardeniz.08@hotmail.com>
2020-02-17 15:17:24 +0300

    var renaming

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <thread>
 #include <chrono>
@@ -11,10 +10,10 @@
 using namespace std;
 
 //Global var definitions
-atomic_int atomic_bill[NUM_BILLS];
+atomic_int bill_atomic[NUM_BILLS];
 pthread_mutex_t atm_mutex[NUM_ATM];
-pthread_mutex_t atm_serve_mutex[NUM_ATM];
-pthread_mutex_t atm_customer_mutex[NUM_ATM];
+pthread_mutex_t atm_serving_mutex[NUM_ATM];
+pthread_mutex_t atm_served_mutex[NUM_ATM];
 pthread_mutex_t stream_mutex;
 customer *atm_service[NUM_ATM];
 pthread_t atm_threads[NUM_ATM];
@@ -50,19 +49,27 @@ int main(int argc, char **argv) {
         goto cleanup;
     }
 
+    //Initially set all bills payments to zero
+    for (auto & i : bill_atomic) {
+        i.store(0);
+    }
+
+    //Create the ATM
     for (int i = 0; i < NUM_ATM; ++i) {
-        pthread_mutex_lock(&atm_serve_mutex[i]);
-        pthread_mutex_lock(&atm_customer_mutex[i]);
+        pthread_mutex_lock(&atm_serving_mutex[i]);
+        pthread_mutex_lock(&atm_served_mutex[i]);
         atm[i] = new struct atm(i, &stream);
         pthread_create(&atm_threads[i], nullptr, atm_handler, (void *) atm[i]);
     }
 
     customer_threads = new pthread_t[num_cust];
 
+    //Create customers
     for (int i = 0; i < num_cust; ++i) {
         pthread_create(&customer_threads[i], nullptr, customer_handler, (void *) &customers[i]);
     }
 
+    //Wait customers
     for (int i = 0; i < num_cust; ++i) {
         pthread_join(customer_threads[i], nullptr);
     }
@@ -70,23 +77,24 @@ int main(int argc, char **argv) {
     //After every customer is served, we can cancel atm threads.
     for (int i = 0; i < NUM_ATM; ++i) {
         pthread_cancel(atm_threads[i]);
-        pthread_mutex_unlock(&atm_serve_mutex[i]);
+        pthread_mutex_unlock(&atm_serving_mutex[i]);
         pthread_join(atm_threads[i], nullptr);
     }
 
     //Write the output
-
     stream << "All payments are completed." << endl;
-    stream << bill_types[CABLE_TV] << ": " << to_string(atomic_bill[CABLE_TV]) << "TL" << endl;
-    stream << bill_types[ELECTRICITY] << ": " << to_string(atomic_bill[ELECTRICITY]) << "TL" << endl;
-    stream << bill_types[GAS] << ": " << to_string(atomic_bill[GAS]) << "TL" << endl;
-    stream << bill_types[TELECOMM] << ": " << to_string(atomic_bill[TELECOMM]) << "TL" << endl;
-    stream << bill_types[WATER] << ": " << to_string(atomic_bill[WATER]) << "TL" << endl;
+    stream << bill_types[CABLE_TV] << ": " << to_string(bill_atomic[CABLE_TV]) << "TL" << endl;
+    stream << bill_types[ELECTRICITY] << ": " << to_string(bill_atomic[ELECTRICITY]) << "TL" << endl;
+    stream << bill_types[GAS] << ": " << to_string(bill_atomic[GAS]) << "TL" << endl;
+    stream << bill_types[TELECOMM] << ": " << to_string(bill_atomic[TELECOMM]) << "TL" << endl;
+    stream << bill_types[WATER] << ": " << to_string(bill_atomic[WATER]) << "TL" << endl;
 
     cleanup:
-
     delete[] customers;
     delete[] customer_threads;
+    for (auto & i : atm) {
+        delete i;
+    }
 
     return 0;
 }
@@ -101,9 +109,9 @@ void *customer_handler(void *_c) {
     // Give our service description to atm
     atm_service[cst->atm] = cst;
     //Let the atm serve us.
-    pthread_mutex_unlock(&atm_serve_mutex[cst->atm]);
+    pthread_mutex_unlock(&atm_serving_mutex[cst->atm]);
     //Check if atm is finished our service.
-    pthread_mutex_lock(&atm_customer_mutex[cst->atm]);
+    pthread_mutex_lock(&atm_served_mutex[cst->atm]);
     //Let other customers use this atm
     pthread_mutex_unlock(&atm_mutex[cst->atm]);
 
@@ -117,20 +125,24 @@ void *atm_handler(void *_a) {
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (true) {
         //Check if any customer is pending
-        pthread_mutex_lock(&atm_serve_mutex[atm->id]);
+        pthread_mutex_lock(&atm_serving_mutex[atm->id]);
         //Check if we should terminate our thread.
         pthread_testcancel();
         //Get customer service description
         customer *cst = atm_service[atm->id];
         // Pay the bills
-        atomic_bill[cst->bill_type].fetch_add(cst->payment);
+        bill_atomic[cst->bill_type].fetch_add(cst->payment);
         //Log the payment
-        string s = "Customer" + to_string(cst->id) + ',' + to_string(cst->payment) + "TL," + bill_types[cst->bill_type] +'\n';
+        string s =
+                "Customer" + to_string(cst->id) + ',' + to_string(cst->payment) + "TL," + bill_types[cst->bill_type] +
+                '\n';
+        //Lock the output stream
         pthread_mutex_lock(&stream_mutex);
         *atm->stream << s;
+        //Unlock the output stream
         pthread_mutex_unlock(&stream_mutex);
-        //Inform the customer about completed service.
-        pthread_mutex_unlock(&atm_customer_mutex[atm->id]);
+        //Inform the customer about service completion.
+        pthread_mutex_unlock(&atm_served_mutex[atm->id]);
     }
 #pragma clang diagnostic pop
 
@@ -148,7 +160,6 @@ int read_file(char *file, int &num_customers, customer *&customers) {
     num_customers = stoi(word);
 
     customers = new customer[num_customers];
-
 
     for (int i = 0; i < num_customers; ++i) {
         customers[i].id = i + 1;
